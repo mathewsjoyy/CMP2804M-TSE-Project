ecdd65d8afe83633d181f8599e966eb28d7d0783
mathews <mathews2492@gmail.com>
2022-04-04 12:14:21 +0100

    fixed pylint issues for previous commit

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 errors = Blueprint('errors', __name__, template_folder='../templates', static_folder='../static')
 
 # For custom error pages
-@errors.app_errorhandler(404) # Specify what error to handle
+@errors.app_errorhandler(404)   # Specify what error to handle
 def error_404(error):
     return render_template('site-errors/404.html'), 404
 
